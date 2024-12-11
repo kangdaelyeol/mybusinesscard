ffd69c37e55fe410b1f5b8a036c15a40a0fc813d
rkdeofuf <asa47972000@naver.com>
2024-12-11 19:21:50 +0900

    update Footer Component style

--- renew/src/components/Footer.jsx
+++ renew/src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="bg-color-cyan w-full flex justify-center items-center">
+        <footer className="py-[30px] bg-color-cyan w-full flex justify-center items-center">
             <a
                 href="https://github.com/kangdaelyeol"
                 target="_blank"
-                className="text-[15px] text-white hover:underline"
+                className="font-bold text-[15px] text-white hover:underline"
             >
                 rkdeofuf
             </a>
