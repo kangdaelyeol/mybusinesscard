1bbf89f9217daabf0daac84180133f80b90afa1e
rkdeofuf <asa47972000@naver.com>
2024-12-11 20:17:41 +0900

    add CardMaker Component
    기존 카드를 보여주는 컴포넌트와 구조가 유사하여 후에 중복된 코드를 리펙토링할 예정

---
+++ renew/src/components/CardMaker.jsx
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react'
+
+export default function CardMaker() {
+    const fileInputRef = useRef()
+    const handleFileInput = (e) => {
+        console.log(fileInputRef.current.files)
+    }
+    const clickFileInput = () => {
+        fileInputRef && fileInputRef.current.click()
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div className="flex flex-1 justify-center items-center h-[230px]">
+            <form
+                className="flex flex-col gap-[10px] w-[400px]"
+                encType="multipart/form-data"
+                onSubmit={handleFormSubmit}
+            >
+                <div className="flex w-full gap-[10px]">
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                        className="grow rounded-[5px] px-[10px] py-[5px] bg-gray-800 outline-none text-white"
+                    />
+                    <select
+                        name="color"
+                        id="color"
+                        className="rounded-[5px] px-[10px] py-[5px] bg-gray-800 outline-none text-white"
+                    >
+                        <option value="black">black</option>
+                        <option value="pink">pink</option>
+                    </select>
+                </div>
+
+                <textarea
+                    className="w-[400px] rounded-[5px] px-[10px] py-[5px] mx-auto resize-none outline-none text-white bg-gray-800"
+                    name="description"
+                    id="description"
+                    rows="3"
+                    placeholder="description"
+                ></textarea>
+
+                <div className="flex w-full gap-[10px]">
+                    <input
+                        onChange={handleFileInput}
+                        accept="image/*"
+                        ref={fileInputRef}
+                        type="file"
+                        className="hidden"
+                    />
+                    <button
+                        onClick={clickFileInput}
+                        className="font-bold py-[10px] rounded-[5px] text-white bg-gray-700 select-none hover:bg-gray-600 grow"
+                    >
+                        file
+                    </button>
+                    <button className="font-bold px-[15px] py-[10px] rounded-[5px] text-white bg-gray-700 select-none hover:bg-gray-600">
+                        save
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
--- renew/src/components/Main.jsx
+++ renew/src/components/Main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import CardEditor from './CardEditor'
 import CardDisplay from './CardDisplay'
-
+import CardMaker from './CardMaker'
+import CardMakerDisplay from './CardMakerDisplay'
 export default function Main() {
     return (
         <div className="bg-gray-900">
-            <div className="max-w-[1100px] mx-auto flex">
-                <CardEditor />
-                <CardDisplay />
+            <div className="max-w-[1100px] mx-auto flex flex-col">
+                <div className="flex">
+                    <CardEditor />
+                    <CardDisplay />
+                </div>
+                <div className="flex">
+                    <CardMaker />
+                    <CardMakerDisplay />
+                </div>
             </div>
         </div>
     )
