e583adcfc509bf443eed480eff47e82a7106ef86
rkdeofuf <asa47972000@naver.com>
2024-12-15 19:35:50 +0900

    update CardDisplayComponent
    업로드한 사진이 보여지는 것을 확인

--- renew/src/components/CardDisplay.jsx
+++ renew/src/components/CardDisplay.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-export default function CardDisplay({ name, theme, description }) {
+const defaultProfileURL = 'https://avatars.githubusercontent.com/u/27201345?v=4'
+
+export default function CardDisplay({ name, theme, description, profile }) {
     return (
         <div className="h-[230px] flex flex-1">
             <div className="w-[80%] m-auto flex gap-[20px] rounded-[20px] border-[3px] border-solid border-color-card p-[35px] bg-transparent">
                 <img
                     alt="avatar"
-                    src="https://avatars.githubusercontent.com/u/27201345?v=4"
+                    src={profile || defaultProfileURL}
                     className="rounded-[50%] w-[120px] h-[120px]"
                 />
                 <div>
--- renew/src/components/CardMakerDisplay.jsx
+++ renew/src/components/CardMakerDisplay.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { CardContext } from '../context/CardContext'
 
+const defaultProfileURL = 'https://avatars.githubusercontent.com/u/27201345?v=4'
+
 export default function CardDisplay() {
     const { state } = useContext(CardContext)
     return (
@@ -8,7 +10,7 @@ export default function CardDisplay() {
             <div className="w-[80%] m-auto flex gap-[20px] rounded-[20px] border-[3px] border-solid border-color-card p-[35px] bg-transparent">
                 <img
                     alt="avatar"
-                    src="https://avatars.githubusercontent.com/u/27201345?v=4"
+                    src={state.profile || defaultProfileURL}
                     className="rounded-[50%] w-[120px] h-[120px]"
                 />
                 <div>
