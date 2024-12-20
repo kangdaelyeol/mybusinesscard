6df87b08addc373449c03443f7b3b86d16ca8de5
rkdeofuf <asa47972000@naver.com>
2024-12-21 01:12:13 +0900

    update Mainpage display
    main page의 최소 높이를 viewport에 맞춰 설정.
    header, footer의 높이를 고정 크기로 설정

--- renew/src/components/Main.jsx
+++ renew/src/components/Main.jsx
@@ -8,7 +8,7 @@ import { CardProvider } from '../context/CardContext'
 export default function Main() {
     const state = useSelector((state) => state.cards)
     return (
-        <div className="bg-gray-900">
+        <div className="bg-gray-900 py-[100px] min-h-[100vh] mb-[-70px]">
             <div className="max-w-[1100px] mx-auto flex flex-col">
                 {state.cards.map((card) => (
                     <div key={card.id} className="flex">
