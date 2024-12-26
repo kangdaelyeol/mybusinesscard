596e3d7ddc1b3ea1fd939a3032f30be470bd3eb6
rkdeofuf <asa47972000@naver.com>
2024-12-26 19:36:04 +0900

    add signup API
    Regex를 통한 validation(입력값 확인) 단계 구현

--- renew/src/api/index.js
+++ renew/src/api/index.js
@@ -6,7 +6,7 @@ const CLOUDINARY_UPLOAD_REQUEST_URL =
 const CLOUDINARY_DELETE_REQUEST_URL =
     'https://api.cloudinary.com/v1_1/dfvqmpyji/asset/destroy'
 
-const LOGIN_REQUEST_URL = 'http://localhost:5000/user'
+const USER_REQUEST_URL = 'http://localhost:5000/user'
 
 export const uploadCloudinaryImage = async (file) => {
     const formData = new FormData()
@@ -30,7 +30,7 @@ export const deleteCloudinaryImage = (signature, assetId) => {
 }
 
 export const userLogin = async (username, password) => {
-    const res = await fetch(LOGIN_REQUEST_URL)
+    const res = await fetch(USER_REQUEST_URL)
 
     const json = await res.json()
 
@@ -38,4 +38,56 @@ export const userLogin = async (username, password) => {
     if (!user) return { status: 400 }
     if (user.password === password) return { status: 200, data: user }
     else return { status: 400 }
+}
+
+export const userSignup = async (username, password, confirmPassword) => {
+    if (!/^[A-Za-z0-9_-]{4,20}$/.test(username)) {
+        return {
+            status: 400,
+            reason: "username should have 4 to 20 characters, shouldn't contain blank(nbsp) and special symbols.",
+        }
+    }
+
+    if (!/^[\w.%+-]{4,20}$/.test(password)) {
+        return {
+            status: 400,
+            reason: "password should have 4 to 20 characters, shouldn't contain blank(nbsp)",
+        }
+    }
+
+    if (password !== confirmPassword) {
+        return {
+            status: 400,
+            reason: "Password doesn't match confirm password correctly. Please check your password again",
+        }
+    }
+
+    const userList = await (await fetch(USER_REQUEST_URL)).json()
+
+    if (userList.find((user) => user.id === username)) {
+        return {
+            status: 400,
+            reason: 'username exists already!',
+        }
+    }
+
+    const userData = await (
+        await fetch(USER_REQUEST_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: username,
+                password,
+                profile: '',
+                cards: [],
+            }),
+        })
+    ).json()
+
+    return {
+        status: 200,
+        value: userData,
+    }
 }
