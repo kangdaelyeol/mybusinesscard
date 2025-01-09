b7e177bd6db0f8037e394844231adfa854ce55c9
rkdeofuf <asa47972000@naver.com>
2025-01-10 01:18:55 +0900

    add profileDetail Component - lightmode theme

--- renew/src/components/ProfileDetail.jsx
+++ renew/src/components/ProfileDetail.jsx
@@ -2,6 +2,9 @@ import AvatarSizing from './AvatarSizing'
 import ImgDisplay from './ImgDisplay'
 import LoadingSpinner from './LoadingSpinner'
 import useProfileDetail from '../hooks/useProfileDetail'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+import classNames from 'classnames'
 
 const PROFILE_DETAIL_IMG_SIZE = 100
 
@@ -19,15 +22,34 @@ export default function ProfileDetail({ handleLogoutClick }) {
         fileLoading,
     } = useProfileDetail()
 
+    const { theme } = useContext(ThemeContext)
+
+    console.log(theme)
     return (
-        <div className="absolute flex flex-col gap-[10px] items-center bg-color-black-light top-[var(--header-height)] right-0 rounded-[30px] w-[400px] mr-[10px] mt-[10px] p-[15px]">
+        <div
+            className={classNames(
+                'absolute flex flex-col gap-[10px] items-center top-[var(--header-height)] right-0 rounded-[30px] w-[400px] mr-[10px] mt-[10px] p-[15px]',
+                {
+                    'bg-color-black-light': theme === 'dark',
+                    'bg-color-white shadow-2xl': theme === 'light',
+                },
+            )}
+        >
             <div className="relative">
                 <ImgDisplay
                     size={PROFILE_DETAIL_IMG_SIZE}
                     profile={userState.profile}
                 />
                 <div
-                    className="absolute bottom-[-8px] right-[-8px] flex justify-center items-center w-[30px] h-[30px] bg-color-black rounded-[50%] cursor-pointer hover:bg-color-black-bright"
+                    className={classNames(
+                        'absolute bottom-[-8px] right-[-8px] flex justify-center items-center w-[30px] h-[30px] rounded-[50%] cursor-pointer',
+                        {
+                            'bg-color-black hover:bg-color-black-bright':
+                                theme === 'dark',
+                            'bg-color-blue hover:bg-color-blue-light text-color-white':
+                                theme === 'light',
+                        },
+                    )}
                     onClick={handleEditProfileClick}
                 >
                     <span className="material-symbols-outlined text-[20px]">
@@ -35,10 +57,27 @@ export default function ProfileDetail({ handleLogoutClick }) {
                     </span>
                 </div>
                 {avatarOption && (
-                    <div className="flex flex-col absolute bottom-[-8px] right-[-110px] bg-color-black w-[100px] rounded-[10px] overflow-hidden">
+                    <div
+                        className={classNames(
+                            'flex flex-col absolute bottom-[-8px] right-[-110px] w-[100px] rounded-[10px] overflow-hidden font-semibold',
+                            {
+                                'bg-color-black': theme === 'dark',
+                                'bg-color-white-light shadow-2xl':
+                                    theme === 'light',
+                            },
+                        )}
+                    >
                         <div
                             onClick={handleNewFileClick}
-                            className="text-center py-[5px] cursor-pointer hover:bg-color-black-bright"
+                            className={classNames(
+                                'text-center py-[5px] cursor-pointer',
+                                {
+                                    'hover:bg-color-black-bright':
+                                        theme === 'dark',
+                                    'hover:bg-color-gray-light':
+                                        theme === 'light',
+                                },
+                            )}
                         >
                             {fileLoading ? <LoadingSpinner /> : 'New File'}
                         </div>
@@ -49,7 +88,15 @@ export default function ProfileDetail({ handleLogoutClick }) {
                             onInput={handleFileInput}
                         />
                         <div
-                            className="text-center py-[5px] cursor-pointer hover:bg-color-black-bright"
+                            className={classNames(
+                                'text-center py-[5px] cursor-pointer',
+                                {
+                                    'hover:bg-color-black-bright':
+                                        theme === 'dark',
+                                    'hover:bg-color-gray-light':
+                                        theme === 'light',
+                                },
+                            )}
                             onClick={handleEditPositionClick}
                         >
                             Position
@@ -61,23 +108,57 @@ export default function ProfileDetail({ handleLogoutClick }) {
                 안녕하세요, {userState.username}님
             </div>
 
-            <div className="py-[7px] w-[150px] font-bold text-center border-solid border-[1px] border-color-white rounded-[9999px] cursor-pointer text-color-skyblue hover:bg-color-black-bright">
+            <div
+                className={classNames(
+                    'py-[7px] w-[150px] font-bold text-center border-solid border-[1px] rounded-[9999px] cursor-pointer',
+                    {
+                        'text-color-skyblue hover:bg-color-black-bright border-color-white':
+                            theme === 'dark',
+                        'text-color-white bg-color-blue hover:bg-color-blue-light border-color-blue-light':
+                            theme === 'light',
+                    },
+                )}
+            >
                 계정 관리
             </div>
 
             <div
                 onClick={handleLogoutClick}
-                className="py-[10px] w-[300px] text-center bg-color-black cursor-pointer hover:bg-color-black-bright rounded-[8px]"
+                className={classNames(
+                    'py-[10px] w-[300px] font-bold text-center cursor-pointer rounded-[8px]',
+                    {
+                        'bg-color-black hover:bg-color-black-bright':
+                            theme === 'dark',
+                        'bg-color-black-light hover:bg-color-black-bright text-color-white':
+                            theme === 'light',
+                    },
+                )}
             >
                 로그아웃
             </div>
 
-            <div className="flex gap-[5px]  items-center">
-                <div className="px-[5px] py-[3px] text-[12px] cursor-pointer hover:bg-color-black-bright">
+            <div className="flex gap-[5px] items-center">
+                <div
+                    className={classNames(
+                        'rounded-[5px] px-[5px] py-[3px] text-[12px] cursor-pointer',
+                        {
+                            'hover:bg-color-black-bright': theme === 'dark',
+                            'hover:bg-color-white-light': theme === 'light',
+                        },
+                    )}
+                >
                     개인정보처리방침
                 </div>
                 <div className="w-[3px] h-[3px] rounded-[50%] bg-color-gray"></div>
-                <div className="px-[5px] py-[3px] text-[12px] cursor-pointer hover:bg-color-black-bright">
+                <div
+                    className={classNames(
+                        'rounded-[5px] px-[5px] py-[3px] text-[12px] cursor-pointer',
+                        {
+                            'hover:bg-color-black-bright': theme === 'dark',
+                            'hover:bg-color-white-light': theme === 'light',
+                        },
+                    )}
+                >
                     서비스 약관
                 </div>
             </div>
