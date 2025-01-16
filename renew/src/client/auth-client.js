597c960b2a18fadb80f0717f20a59d6ecec3de89
rkdeofuf <asa47972000@naver.com>
2025-01-16 22:00:54 +0900

    add auth-client - changePassword API

--- renew/src/client/auth-client.js
+++ renew/src/client/auth-client.js
@@ -1,5 +1,5 @@
 import { db } from '../service/firebase'
-import { ref, get, child } from 'firebase/database'
+import { ref, get, child, set } from 'firebase/database'
 
 export const authClient = {
     signIn: async (username, password) => {
@@ -10,7 +10,7 @@ export const authClient = {
             }
         }
 
-        if (!/^[\w!@#$%^&*()]{4,20}$/.test(password)) {
+        if (!/^[\w!@#$%^&*()+=]{4,20}$/.test(password)) {
             return {
                 status: 400,
                 reason: "password has 4 to 20 length of characters and doesn't contain blank",
@@ -32,6 +32,51 @@ export const authClient = {
         } catch (e) {
             console.log(e)
             return { status: 400, reason: 'error' }
+        }
+    },
+
+    changePassword: async (
+        username,
+        password,
+        newPassword,
+        confirmPassword,
+    ) => {
+        if (!/^[\w!@#$%^&*()+=]{4,20}$/.test(password)) {
+            return {
+                status: 400,
+                reason: "password has 4 to 20 length of characters and doesn't contain blank",
+            }
+        }
+
+        if (newPassword !== confirmPassword) {
+            return {
+                status: 400,
+                reason: "new password doesn't match confirm password",
+            }
+        }
+
+        try {
+            const userPasswordRef = ref(db, `users/${username}/password`)
+            const pw = await get(userPasswordRef)
+
+            if (pw.val() !== password) {
+                return {
+                    status: 400,
+                    reason: "password doesn't match current password",
+                }
+            }
+
+            if (password === newPassword) {
+                return {
+                    status: 400,
+                    reason: "you can't change password into the same password",
+                }
+            }
+
+            await set(userPasswordRef, password)
+            return { status: 200 }
+        } catch (e) {
+            return { status: 400, reason: e }
         }
     },
 }
