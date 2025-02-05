5c0f1dcf74b2c3c2c915aa33f33ffbf16a578685
rkdeofuf <asa47972000@naver.com>
2025-02-05 21:52:07 +0900

    add generateSignatureSHA1
    서명을 생성하는 유틸리티 메서드 추가

---
+++ renew/src/utils/generateSignatureSHA1.js
@@ -0,0 +1,8 @@
+import sha256 from 'crypto-js/sha256'
+
+const generateSignatureSHA1 = (publicId, apiSecret) => {
+    const timestamp = new Date().getTime()
+    return sha256(`public_id=${publicId}&timestamp=${timestamp}${apiSecret}`)
+}
+
+export default generateSignatureSHA1
