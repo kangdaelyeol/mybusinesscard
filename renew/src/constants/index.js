04e2ab092ab2f7d70d51dc7cefba5d253c3e97b1
rkdeofuf <asa47972000@naver.com>
2025-02-06 19:27:47 +0900

    modularization constants

--- renew/src/constants/index.js
+++ renew/src/constants/index.js
@@ -1,33 +1,5 @@
-export const DEFAULT_PROFILE_URL =
-    'https://avatars.githubusercontent.com/u/27201345?v=4'
-
-export const DEFAULT_CARD_PROFILE_URL =
-    'https://avatars.githubusercontent.com/u/27201345?v=4'
-
-export const GITHUB_URL = 'https://github.com/kangdaelyeol'
-
-export const MAX_PROFILE_SIZE = 3000000
-
-export const CARD_IMAGE_SIZE = 120
-
-export const PICTURE_BOX_SIZE = 400
-
-export const PICTURE_BOX_SIZE_MEDIUM = 250
-
-export const MAX_SCALE_VALUE = 5
-
-export const RATE_BAR_WIDTH = 400
-
-export const RATE_BAR_WIDTH_MEDIUM = 250
-
-export const PROFILE_DETAIL_IMG_SIZE = 100
-
-export const CLOUDINARY_UPLOAD_REQUEST_URL =
-    'https://api.cloudinary.com/v1_1/dfvqmpyji/image/upload'
-
-export const CLOUDINARY_DELETE_REQUEST_URL =
-    'https://api.cloudinary.com/v1_1/dfvqmpyji/image/destroy'
-
-export const LOCALSTORAGE_TOKEN_NAME = 'USER_NAME_BUSINESS_CARD'
-
-export const TOAST_MESSAGE_TIMER = 1000
+export * from './defaultUrl'
+export * from './localStorage'
+export * from './requestUrl'
+export * from './ui-internal'
+export * from './ui-size'
