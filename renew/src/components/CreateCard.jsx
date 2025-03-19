dffb719e99a46a9773b0d2f67a43a9f657ad3b87
rkdeofuf <asa47972000@naver.com>
2025-03-19 16:19:57 +0900

    fix CreateCard Component UI
    Exit 버튼을 viewpoint에 고정하는 것이 아닌 컴포넌트에 고정되도록 수정(fixed -> relative)

--- renew/src/components/CreateCard.jsx
+++ renew/src/components/CreateCard.jsx
@@ -11,23 +11,25 @@ export default function CreateCard() {
     const hideCreateCard = () => {
         publish(EVENT_TYPES.HIDE_CREATE_CARD)
     }
-    
+
     return (
         <div className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-10 bg-color-black/90">
-            <div className="text-color-white text-[30px] font-semibold">
-                Create Your Card!
-            </div>
+            <div className="relative flex flex-col">
+                <div className="text-color-white text-[30px] text-center font-semibold">
+                    Create Your Card!
+                </div>
 
-            <div className="flex gap-[20px] max-medium:flex-col-reverse mt-[30px]">
-                <CardProvider>
-                    <CardMaker />
-                    <CardDisplay />
-                </CardProvider>
+                <div className="flex gap-[20px] max-medium:flex-col-reverse mt-[30px]">
+                    <CardProvider>
+                        <CardMaker />
+                        <CardDisplay />
+                    </CardProvider>
+                </div>
+                <div
+                    onClick={hideCreateCard}
+                    className="absolute top-[5px] right-[30px] w-[30px] h-[30px] rounded-[50%] bg-red-500 hover:bg-red-400 cursor-pointer"
+                ></div>
             </div>
-            <div
-                onClick={hideCreateCard}
-                className="fixed top-[30px] right-[30px] w-[30px] h-[30px] rounded-[50%] bg-red-500 hover:bg-red-400 cursor-pointer"
-            ></div>
         </div>
     )
 }
